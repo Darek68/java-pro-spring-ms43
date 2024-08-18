97eac30d99b09d8eafb2dba19b0554b9f977a827
Darek <darek68@list.ru>
2024-08-18 20:01:31 +0300

    hw43CommitInitSQLadapted

--- java-pro-spring-lesson6/dev/init-transfers-history.sql
+++ java-pro-spring-lesson6/dev/init-transfers-history.sql
@@ -4,21 +4,20 @@ DROP TABLE IF EXISTS transfers;
 create table transfers
 (
     id              bigserial primary key,
-    request_id      bigserial,
-    sender_id       bigint,
-    send_account    varchar(16),
-    receiver_id     bigint,
-    receiv_account  varchar(16),
-    amount          numeric(6, 2),
-    document_status varchar(16),
-    update_date_time timestamp
+    client_id_from  bigint,
+    client_id_to     bigint,
+    account_number_from  varchar(16),
+    account_number_to  varchar(16),
+    transfer_sum          numeric(6, 2),
+    status varchar(16),
+    created_at timestamp,
+    updated_at timestamp
 );
-
 
 CREATE OR REPLACE FUNCTION RandomStatus(OUT result text) as
 $BODY$
 BEGIN
-    IF random() > 0.3 THEN result := 'COMPLETED';
+    IF random() > 0.3 THEN result := 'SUCCESS';
     ELSEIF random() > 0.1 THEN result := 'CREATED';
     ELSE result := 'ERROR';
     END IF;
@@ -26,15 +25,15 @@ BEGIN
 END
 $BODY$ language plpgsql;
 
-INSERT INTO transfers (id,request_id, sender_id, send_account,receiver_id,receiv_account,amount,document_status,update_date_time)
+INSERT INTO transfers (id, client_id_from, client_id_to,account_number_from,account_number_to,transfer_sum,status,created_at,updated_at)
 SELECT s,
-       trunc(random()*10000 + 1),
+       trunc(random()*10 + 1),
        trunc(random()*10 + 1),
        trunc(random()*(8999999999999999) + 1000000000000000)::bigint::text::varchar,
-       trunc(random()*10 + 1),
        trunc(random()*(8999999999999999) + 1000000000000000)::bigint::text::varchar,
        trunc(random()*(1000))::numeric(6,2),
        RandomStatus(),
+       timestamp '2024-01-01 00:00:00' + random() * (timestamp '2024-08-18 20:00:00' - timestamp '2024-01-01 00:00:00'),
        timestamp '2024-01-01 00:00:00' + random() * (timestamp '2024-08-18 20:00:00' - timestamp '2024-01-01 00:00:00')
 FROM generate_series(1,100) s;
 
